42e84e623dcc740a83b22abe71911094beac8710
Lucas Comamala <lucascomamala@gmail.com>
2022-09-15 16:39:27 -0600

    Fix linter errors

--- src/pages/QuotePage.js
+++ src/pages/QuotePage.js
@@ -1,5 +1,11 @@
 import React from 'react';
 
-const QuotePage = () => <blockquote>“Do not worry about your difficulties in mathematics. I can assure you mine are still greater.”<br /> – Albert Einstein</blockquote>;
+const QuotePage = () => (
+  <blockquote>
+    “Do not worry about your difficulties in mathematics. I can assure you mine are still greater.
+    ”
+    <br />
+    – Albert Einstein
+  </blockquote>);
 
 export default QuotePage;
